e52a54bf9e4f8ef083129e3356505e992f637332
Matthal <mattiarbc@gmail.com>
2018-10-15 12:00:08 +0200

    Animal list implementation

--- app/src/main/java/com/fao/fmd/fmdappbeta/FarmCreation.java
+++ app/src/main/java/com/fao/fmd/fmdappbeta/FarmCreation.java
@@ -1,7 +1,11 @@
 package com.fao.fmd.fmdappbeta;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FarmCreation extends Activity {
 
@@ -9,5 +13,16 @@ public class FarmCreation extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_creation);
+
+        Button cFarm = findViewById(R.id.createFarm);
+
+        cFarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Diagnosis algorithm in development",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/fao/fmd/fmdappbeta/FarmList.java
+++ app/src/main/java/com/fao/fmd/fmdappbeta/FarmList.java
@@ -6,16 +6,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FarmList extends Activity implements AdapterView.OnItemSelectedListener {
 
-    ListView animalList;
+    AnimalListAdapter listAdapter;
+    ExpandableListView animalList;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +29,13 @@ public class FarmList extends Activity implements AdapterView.OnItemSelectedList
         Spinner spinner = findViewById(R.id.farmSpinner);
         animalList = findViewById(R.id.animalList);
 
-        List<String> Array = new ArrayList<>();
-        Array.add("farm 1");
-        Array.add("farm 2");
-        Array.add("farm 3");
+        List<String> farmList = new ArrayList<>();
+        farmList.add("farm 1");
+        farmList.add("farm 2");
+        farmList.add("farm 3");
 
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,Array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,farmList);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
@@ -48,18 +53,38 @@ public class FarmList extends Activity implements AdapterView.OnItemSelectedList
 
 
         if(farmName.equals("farm 2")){
-            List<String> Array = new ArrayList<>();
-            Array.add("animal 1");
-            Array.add("animal 2");
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(),
-                    android.R.layout.simple_list_item_1, Array);
-            animalList.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
+            // preparing list data
+            prepareListData();
+
+            listAdapter = new AnimalListAdapter(this, listDataHeader, listDataChild);
+
+            // setting list adapter
+            animalList.setAdapter(listAdapter);
+            listAdapter.notifyDataSetChanged();
         }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // do nothing
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        String[] ID = {"201","202","203"};
+
+        listDataHeader.add("Animal 1");
+        listDataHeader.add("Animal 2");
+        listDataHeader.add("Animal 3");
+
+        for (int i = 0; i < ID.length; i++){
+            List<String> top250 = new ArrayList<String>();
+            // Adding child data ;
+            top250.add("ID : " + ID[i] + "\n" +  "Name : " + "Boar");
+            listDataChild.put(listDataHeader.get(i), top250);
+        }
+
     }
 
 
